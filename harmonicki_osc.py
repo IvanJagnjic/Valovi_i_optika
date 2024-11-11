46ca802be57898b7682cc33f08f1324a8181de8e
IvanJagnjic <161047295+IvanJagnjic@users.noreply.github.com>
2024-11-11 22:16:20 +0100

    Add files via upload

--- harmonicki_osc.py
+++ harmonicki_osc.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 
 class Oscilator:
-    def __init__(self, k, x0, brzina, dt, m,koef_otpora,vrsta_otpora):
+    def __init__(self, k, x0, brzina, dt, m,koef_otpora):
         self.brzina = brzina
         self.x0 = x0
         self.brzina = brzina
@@ -19,7 +19,6 @@ class Oscilator:
         self.dt = dt
         self.m = m
         self.koef_otpora=koef_otpora
-        self.vrsta_otpora=vrsta_otpora
 
     def move(self, dt):
         self.x0 = self.x0 + dt * self.brzina
@@ -30,7 +29,7 @@ class Oscilator:
         pass
 
 
-    def graf(self):
+    def graf(self,n):
         poc_brzina = self.brzina
         poc_x0 = self.x0
 
@@ -42,7 +41,7 @@ class Oscilator:
         lista_v = []
 
         while period < 10:
-            otpor =m.fabs(pow(self.brzina, self.vrsta_otpora))*self.koef_otpora
+            otpor =m.fabs(pow(self.brzina,n)*self.koef_otpora)
             Fel = -1 * self.k*self.x0-np.sign(self.brzina)*otpor
             a = Fel / self.m
             self.move(self.dt)
@@ -83,21 +82,28 @@ class Oscilator:
         return (2 * m.pi * m.sqrt(float(self.m) / self.k))
 
     def prikazi_graf(self):
-        [y1, y2, y3, x] = self.graf()
+        [y1, y2, y3, x] = self.graf(1)
+        [y12, y22, y32, x2] = self.graf(2)
 
         plt.subplot(1, 3, 1)
-        plt.plot(x, y1)
+        plt.plot(x, y1,label="Ovisnost o v")
+        plt.plot(x2, y12,label="Ovisnost o v^2")
         plt.title("Ovisnost x o vremenu:")
+        plt.legend()
 
         plt.subplot(1, 3, 2)
-        plt.plot(x, y2)
+        plt.plot(x, y2,label="Ovisnost o v")
+        plt.plot(x2, y22,label="Ovisnost o v^2")
         plt.title("Ovisnost v o vremenu:")
+        plt.legend()
 
         plt.subplot(1, 3, 3)
-        plt.plot(x, y3)
+        plt.plot(x, y3,label="Ovisnost o v")
+        plt.plot(x2, y32,label="Ovisnost o v^2")
         plt.title("Ovisnost a o vremenu:")
+        plt.legend()
         plt.show()
 
 
-cestica = Oscilator(150, 6, 15, 0.001, 10,0.25,2)
+cestica = Oscilator(150, 6, 15, 0.001, 10,0.25)
 cestica.prikazi_graf()
